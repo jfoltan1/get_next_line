8f28ed21e9f3a50c1d21e44979851b95f911c265
Julius Foltan <jfoltan@4c0c1.42berlin.de>
2023-04-23 13:47:48 +0200

    small progress on linked lists

--- test/test.c
+++ test/test.c
@@ -1,10 +1,26 @@
 #include "test.h"
+
 int	ft_strlen(char *c)
 {	
 	int	i;
 	i = 0;
 	while (c[i] != '\0')
 		i++;
+	return(i);
+}
+
+int	ft_strcpy(char *src,char *dest)
+{
+	int	i;
+
+	i = 0;
+	dest = ft_calloc(ft_strlen(src),sizeof(char));
+	while (src[i])
+	{	
+		dest[i] = src[i];
+		i++;
+	}
+	write(1, "a", 1);
 	return(i);
 }
 
@@ -15,9 +31,10 @@ char *return_node()
 
 	line = "PLEASE WORK FOR THE LOVE OF GOD.";
 	if (!buffer)
-		buffer = calloc(ft_strlen(line),sizeof(char));
-	buffer -> content = line;
+		buffer = ft_calloc(ft_strlen(line),sizeof(char));
+	ft_strcpy(line,buffer -> content);
 	buffer -> next = NULL;
+	//free(line);
 	return((char*)(buffer->content));
 }
 int main(void)
